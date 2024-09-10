d580f13a8b26bdc71971b7e7ca7606d970ee5751
fanyingfx <fanyingfx@outlook.com>
2024-09-10 21:44:55 +0800

    finished the cpu-exceptions using 'x86-interrupt'

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+// mod serial;
+// mod vga_buffer;
+// static HELLO:&[u8] = b"Hello World!";
+
+use core::panic::PanicInfo;
+use blog_os::println;
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    blog_os::init();
+
+    x86_64::instructions::interrupts::int3();
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    loop {}
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info);
+    loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
