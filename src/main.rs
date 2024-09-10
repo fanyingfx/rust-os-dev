4aa5be7fcae2cefc65fb8b0721b7aa08b32facea
fanyingfx <fanyingfx@outlook.com>
2024-09-10 23:13:28 +0800

    Double Falults

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,15 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
-    x86_64::instructions::interrupts::int3();
+    // x86_64::instructions::interrupts::int3();
+    fn stack_overflow(){
+        stack_overflow();
+    }
+    stack_overflow();
+
+    unsafe {
+        *(0xdeadbeef as *mut u8) = 42;
+    }
 
     #[cfg(test)]
     test_main();
